62efd67b8163d9025c619aadaecbe6d1364bbb06
MaximeVan <maxime.vanbossel@hotmail.fr>
2018-04-04 15:00:35 +0200

    Commit 4 avril Maxime

--- app/src/main/java/com/example/vanbossm/tagoid/MainActivity.java
+++ app/src/main/java/com/example/vanbossm/tagoid/MainActivity.java
@@ -9,12 +9,23 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -50,4 +61,62 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+/*
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+
+        myurl = "https://data.metromobilite.fr/api/routers/default/index/routes";
+        URL url = new URL(myurl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        try {
+            conn.setReadTimeout(10000 // milliseconds
+                    );
+            conn.setConnectTimeout(15000 //milliseconds
+                     );
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String result = readIt(is);
+            return result;
+
+            JSONObject jsonObject = new JSONObject(result);
+            // On récupère le tableau d'objets qui nous concernent
+            JSONArray array = new JSONArray(jsonObject.getString(""));
+            // Pour tous les objets on récupère les infos
+            for (int i = 0; i < array.length(); i++) {
+                // On récupère un objet JSON du tableau
+                JSONObject obj = new JSONObject(array.getString(i));
+                // On fait le lien avec le moyen de transport et l'Objet JSON
+                Id id = new Id();
+                id.setId(obj.getString("id"));
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            }/*finally{
+                if (is != null) {
+                    is.close();
+                    conn.disconnect();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+
+    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line + "\n");
+        }
+        return builder.toString();
+    }*/
 }
