66b6a86e4c7c92ea4370e4c97c5dabac6991994c
MaximeVan <maxime.vanbossel@hotmail.fr>
2018-04-25 14:48:34 +0200

    Notification

--- app/src/main/java/com/example/vanbossm/tagoid/MainActivity.java
+++ app/src/main/java/com/example/vanbossm/tagoid/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.vanbossm.tagoid;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity{
+
+    private static int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,19 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onReceive(Context context, Intent intent) {
                 Log.e("RECEIVER", "Intent received, make a notification here");
+
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.github.com/"));
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+                NotificationCompat.Builder mBuilder =
+                        (NotificationCompat.Builder) new NotificationCompat.Builder(MainActivity.this)
+                                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                                .setContentTitle("Tagoid")
+                                .setContentText("Pwit pwit")
+                                .setContentIntent(pendingIntent);
+
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(001, mBuilder.build());
             }
         };
 
